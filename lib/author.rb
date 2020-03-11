270fe34debecba04e3ed39b75f1334ff1b849606
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-11 18:32:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,19 @@
 class Author
+  attr_accessor :name
 
+  def initialize(name)
+    self.name = name
+  end
+
+  def posts
+    Post.all.select { |post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
 end
